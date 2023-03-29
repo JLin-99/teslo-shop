3b7d50499caea1cadc4d985f68e5af6812b6a2ab
José Lin (Light-it) <jose.lin@lightit.io>
2023-03-29 17:51:10 -0300

    Add Navbar

--- components/layouts/ShopLayout.tsx
+++ components/layouts/ShopLayout.tsx
@@ -2,6 +2,8 @@ import { FC, PropsWithChildren } from "react";
 
 import Head from "next/head";
 
+import { Navbar } from "../ui";
+
 interface Props {
   title: string;
   pageDescription: string;
@@ -26,7 +28,9 @@ export const ShopLayout: FC<PropsWithChildren<Props>> = ({
 
         {imageFullUrl && <meta name="og:image" content={imageFullUrl} />}
       </Head>
-      <nav></nav>
+      <nav>
+        <Navbar />
+      </nav>
 
       <main
         style={{ margin: "80px auto", maxWidth: "1440px", padding: "0px 30px" }}
