047d0ec0fb9d5c7df0475a094dc82dc77b69bbbd
José Lin (Light-it) <jose.lin@lightit.io>
2023-03-29 19:04:45 -0300

    Add temporal HomePage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
-import { ShopLayout } from "@/components/layouts";
-import { Typography } from "@mui/material";
+import {
+  Card,
+  CardActionArea,
+  CardMedia,
+  Grid,
+  Typography,
+} from "@mui/material";
 
-export default function Home() {
+import { ShopLayout } from "@/components/layouts";
+import { initialData } from "@/database/products";
+
+export default function HomePage() {
   return (
     <ShopLayout
       title={"TesloShop - Home"}
@@ -13,6 +21,22 @@ export default function Home() {
       <Typography variant="h2" sx={{ mb: 1 }}>
         All Products
       </Typography>
+
+      <Grid container spacing={4}>
+        {initialData.products.map((product) => (
+          <Grid item xs={6} sm={4} key={product.slug}>
+            <Card>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  image={`products/${product.images[0]}`}
+                  alt={product.title}
+                />
+              </CardActionArea>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
     </ShopLayout>
   );
 }
