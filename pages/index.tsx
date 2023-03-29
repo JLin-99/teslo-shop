0f0977bfd854a5d7f68ce26d319f72cc401cda75
José Lin (Light-it) <jose.lin@lightit.io>
2023-03-29 17:29:01 -0300

    Add ShopLayout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,18 @@
+import { ShopLayout } from "@/components/layouts";
+import { Typography } from "@mui/material";
+
 export default function Home() {
-  return <h1>Teslo-shop</h1>;
+  return (
+    <ShopLayout
+      title={"TesloShop - Home"}
+      pageDescription={"Find the best products from Teslo here!"}
+    >
+      <Typography variant="h1" component="h1">
+        Shop
+      </Typography>
+      <Typography variant="h2" sx={{ mb: 1 }}>
+        All Products
+      </Typography>
+    </ShopLayout>
+  );
 }
