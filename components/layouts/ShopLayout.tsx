204aba535e6423d66c123bb9c1772f7f6bbaa6cc
José Lin (Light-it) <jose.lin@lightit.io>
2023-03-29 19:20:10 -0300

    Add SideMenu

--- components/layouts/ShopLayout.tsx
+++ components/layouts/ShopLayout.tsx
@@ -2,7 +2,7 @@ import { FC, PropsWithChildren } from "react";
 
 import Head from "next/head";
 
-import { Navbar } from "../ui";
+import { Navbar, SideMenu } from "../ui";
 
 interface Props {
   title: string;
@@ -31,6 +31,8 @@ export const ShopLayout: FC<PropsWithChildren<Props>> = ({
       <nav>
         <Navbar />
       </nav>
+
+      <SideMenu />
 
       <main
         style={{ margin: "80px auto", maxWidth: "1440px", padding: "0px 30px" }}
