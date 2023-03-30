9919f43531524cda98defca0abf0a574eac603c3
José Lin (Light-it) <jose.lin@lightit.io>
2023-03-29 21:20:59 -0300

    Add ProductPage

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,11 +1,14 @@
 import { FC, useMemo, useState } from "react";
 
+import NextLink from "next/link";
+
 import {
   Box,
   Card,
   CardActionArea,
   CardMedia,
   Grid,
+  Link,
   Typography,
 } from "@mui/material";
 
@@ -33,14 +36,18 @@ export const ProductCard: FC<Props> = ({ product }) => {
       onMouseLeave={() => setIsHovered(false)}
     >
       <Card>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            image={productImage}
-            alt={product.title}
-            className="fadeIn"
-          />
-        </CardActionArea>
+        <NextLink href="/product/slug" passHref legacyBehavior prefetch={false}>
+          <Link>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                image={productImage}
+                alt={product.title}
+                className="fadeIn"
+              />
+            </CardActionArea>
+          </Link>
+        </NextLink>
       </Card>
 
       <Box sx={{ mt: 1 }} className="fadeIn">
