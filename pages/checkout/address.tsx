24e9ee5e9a68ed727fb136122a826c7492d8a773
José Lin (Light-it) <jose.lin@lightit.io>
2023-03-30 13:19:38 -0300

    Add AddressPage

---
+++ pages/checkout/address.tsx
@@ -0,0 +1,80 @@
+import { ShopLayout } from "@/components/layouts";
+import {
+  Box,
+  Button,
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+} from "@mui/material";
+
+const AddressPage = () => {
+  return (
+    <ShopLayout title="Address" pageDescription="Confirm your shipping address">
+      <Typography variant="h1" component="h1" marginBottom={4}>
+        Address
+      </Typography>
+      <Grid container spacing={2}>
+        <Grid item xs={6} sm={3}>
+          <TextField label="First name (optional)" variant="filled" fullWidth />
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <TextField label="Last name" variant="filled" fullWidth />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField label="Phone (optional)" variant="filled" fullWidth />
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <TextField label="Address" variant="filled" fullWidth />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            label="Apartment, suite, etc. (optional)"
+            variant="filled"
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField label="City" variant="filled" fullWidth />
+        </Grid>
+
+        <Grid item xs={4} sm={2}>
+          <FormControl fullWidth>
+            <InputLabel>Country/Region</InputLabel>
+            <Select variant="filled" value={1}>
+              <MenuItem value={1}>Country 1</MenuItem>
+              <MenuItem value={2}>Country 2</MenuItem>
+              <MenuItem value={3}>Country 3</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={4} sm={2}>
+          <FormControl fullWidth>
+            <InputLabel>State</InputLabel>
+            <Select variant="filled" value={1}>
+              <MenuItem value={1}>State 1</MenuItem>
+              <MenuItem value={2}>State 2</MenuItem>
+              <MenuItem value={3}>State 3</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={4} sm={2}>
+          <TextField label="ZIP code" variant="filled" fullWidth />
+        </Grid>
+
+        <Grid item xs={12} sm={6}></Grid>
+      </Grid>
+
+      <Box marginTop={5} display="flex" justifyContent="center">
+        <Button color="secondary" className="circular-btn" size="large">
+          Check order
+        </Button>
+      </Box>
+    </ShopLayout>
+  );
+};
+export default AddressPage;
