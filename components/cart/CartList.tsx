67e68acc272849e2836d425358f54ebee80c2400
José Lin (Light-it) <jose.lin@lightit.io>
2023-03-30 12:23:42 -0300

    Display products from cart

--- components/cart/CartList.tsx
+++ components/cart/CartList.tsx
@@ -1,6 +1,16 @@
-import { Typography } from "@mui/material";
+import {
+  Button,
+  CardActionArea,
+  CardMedia,
+  Grid,
+  Link,
+  Typography,
+} from "@mui/material";
 
 import { initialData } from "@/database/products";
+import NextLink from "next/link";
+import { Box } from "@mui/system";
+import { ItemCounter } from "../ui";
 
 const productsInCart = [
   initialData.products[0],
@@ -10,8 +20,43 @@ const productsInCart = [
 export const CartList = () => {
   return (
     <>
-      {productsInCart.map((products) => (
-        <Typography key={products.slug}>{products.slug}</Typography>
+      {productsInCart.map((product) => (
+        <Grid container spacing={2} sx={{ mb: 1 }} key={product.slug}>
+          <Grid item xs={3}>
+            <NextLink href="product/slug" passHref legacyBehavior>
+              <Link>
+                <CardActionArea>
+                  <CardMedia
+                    image={`products/${product.images[0]}`}
+                    component="img"
+                    sx={{ borderRadius: "5px" }}
+                  />
+                </CardActionArea>
+              </Link>
+            </NextLink>
+          </Grid>
+          <Grid item xs={7}>
+            <Box display="flex" flexDirection="column">
+              <Typography variant="body1">{product.title}</Typography>
+              <Typography variant="body1">Size:</Typography>
+
+              <ItemCounter />
+            </Box>
+          </Grid>
+          <Grid
+            item
+            xs={2}
+            display="flex"
+            alignItems="center"
+            flexDirection="column"
+          >
+            <Typography variant="subtitle1">${product.price}</Typography>
+
+            <Button variant="text" color="secondary">
+              Remove
+            </Button>
+          </Grid>
+        </Grid>
       ))}
     </>
   );
