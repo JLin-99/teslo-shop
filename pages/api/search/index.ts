897e35b774509f1b5e5f1d9af193d6d5d5b6024c
José Lin (Light-it) <jose.lin@lightit.io>
2023-03-31 12:58:04 -0300

    Add REST endpoint to GET product by search query

---
+++ pages/api/search/index.ts
@@ -0,0 +1,10 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+type Data = { message: string };
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  res.status(400).json({ message: "Specify search query" });
+}
