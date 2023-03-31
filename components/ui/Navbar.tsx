fd10d6d53a6647495215dadd9b5458e85e7c41d8
José Lin (Light-it) <jose.lin@lightit.io>
2023-03-31 20:23:19 -0300

    Show active category on Navbar

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,4 +1,5 @@
 import NextLink from "next/link";
+import { useRouter } from "next/router";
 
 import {
   Button,
@@ -13,6 +14,8 @@ import {
 import { SearchOutlined, ShoppingCartOutlined } from "@mui/icons-material";
 
 export const Navbar = () => {
+  const { asPath } = useRouter();
+
   return (
     <AppBar>
       <Toolbar>
@@ -28,17 +31,23 @@ export const Navbar = () => {
         <Box sx={{ display: { xs: "none", sm: "block" } }}>
           <NextLink href="/category/men" passHref legacyBehavior>
             <Link>
-              <Button>Men</Button>
+              <Button color={asPath === "/category/men" ? "primary" : "info"}>
+                Men
+              </Button>
             </Link>
           </NextLink>
           <NextLink href="/category/women" passHref legacyBehavior>
             <Link>
-              <Button>Women</Button>
+              <Button color={asPath === "/category/women" ? "primary" : "info"}>
+                Women
+              </Button>
             </Link>
           </NextLink>
           <NextLink href="/category/kid" passHref legacyBehavior>
             <Link>
-              <Button>Kid</Button>
+              <Button color={asPath === "/category/kid" ? "primary" : "info"}>
+                Kid
+              </Button>
             </Link>
           </NextLink>
         </Box>
--- database/products.ts
+++ database/products.ts
@@ -35,6 +35,19 @@ export const initialData: SeedData = {
     },
     {
       description:
+        "The Women's Cropped Puffer Jacket features a uniquely cropped silhouette for the perfect, modern style while on the go during the cozy season ahead. The puffer features subtle silicone injected Teslo logos below the back collar and on the right sleeve, custom matte metal zipper pulls and a soft, fleece lined collar. Made from 87% nylon and 13% polyurethane.",
+      images: ["1740535-00-A_0_2000.jpg", "1740535-00-A_1.jpg"],
+      inStock: 85,
+      price: 225,
+      sizes: ["XS", "S", "M"],
+      slug: "women_cropped_puffer_jacket",
+      type: "hoodies",
+      tags: ["hoodie"],
+      title: "Women's Cropped Puffer Jacket",
+      gender: "women",
+    },
+    {
+      description:
         "The Men's Quilted Shirt Jacket features a uniquely fit, quilted design for warmth and mobility in cold weather seasons. With an overall street-smart aesthetic, the jacket features subtle silicone injected Teslo logos below the back collar and on the right sleeve, as well as custom matte metal zipper pulls. Made from 87% nylon and 13% polyurethane.",
       images: ["1740507-00-A_0_2000.jpg", "1740507-00-A_1.jpg"],
       inStock: 5,
@@ -46,7 +59,19 @@ export const initialData: SeedData = {
       title: "Men's Quilted Shirt Jacket",
       gender: "men",
     },
-
+    {
+      description:
+        "Introducing the Teslo Chill Collection. The Women's Chill Half Zip Cropped Hoodie has a premium, soft fleece exterior and cropped silhouette for comfort in everyday lifestyle. The hoodie features an elastic hem that gathers at the waist, subtle thermoplastic polyurethane Teslo logos along the hood and on the sleeve, a double layer single seam hood and a custom ring zipper pull. Made from 60% cotton and 40% recycled polyester.",
+      images: ["1740226-00-A_0_2000.jpg", "1740226-00-A_1.jpg"],
+      inStock: 10,
+      price: 130,
+      sizes: ["XS", "S", "M", "XXL"],
+      slug: "women_chill_half_zip_cropped_hoodie",
+      type: "hoodies",
+      tags: ["hoodie"],
+      title: "Women's Chill Half Zip Cropped Hoodie",
+      gender: "women",
+    },
     {
       description:
         "Introducing the Teslo Raven Collection. The Men's Raven Lightweight Zip Up Bomber has a premium, modern silhouette made from a sustainable bamboo cotton blend for versatility in any season. The hoodie features subtle thermoplastic polyurethane Teslo logos on the left chest and below the back collar, a concealed chest pocket with custom matte zipper pulls and a french terry interior. Made from 70% bamboo and 30% cotton.",
@@ -60,7 +85,19 @@ export const initialData: SeedData = {
       title: "Men's Raven Lightweight Zip Up Bomber Jacket",
       gender: "men",
     },
-
+    {
+      description:
+        "The Kids Scribble T Logo Onesie is made from 100% Peruvian cotton and features a Teslo T sketched logo for every little artist to wear.",
+      images: ["8529387-00-A_0_2000.jpg", "8529387-00-A_1.jpg"],
+      inStock: 0,
+      price: 30,
+      sizes: ["XS", "S"],
+      slug: "scribble_t_logo_onesie",
+      type: "shirts",
+      tags: ["shirt"],
+      title: "Scribble T Logo Onesie",
+      gender: "kid",
+    },
     {
       description:
         "Introducing the Teslo Turbine Collection. Designed for style, comfort and everyday lifestyle, the Men's Turbine Long Sleeve Tee features a subtle, water-based T logo on the left chest and our Teslo wordmark below the back collar. The lightweight material is double-dyed, creating a soft, casual style for ideal wear in any season. Made from 50% cotton and 50% polyester.",
@@ -386,32 +423,7 @@ export const initialData: SeedData = {
       title: "Thermal Cuffed Beanie",
       gender: "unisex",
     },
-    {
-      description:
-        "The Women's Cropped Puffer Jacket features a uniquely cropped silhouette for the perfect, modern style while on the go during the cozy season ahead. The puffer features subtle silicone injected Teslo logos below the back collar and on the right sleeve, custom matte metal zipper pulls and a soft, fleece lined collar. Made from 87% nylon and 13% polyurethane.",
-      images: ["1740535-00-A_0_2000.jpg", "1740535-00-A_1.jpg"],
-      inStock: 85,
-      price: 225,
-      sizes: ["XS", "S", "M"],
-      slug: "women_cropped_puffer_jacket",
-      type: "hoodies",
-      tags: ["hoodie"],
-      title: "Women's Cropped Puffer Jacket",
-      gender: "women",
-    },
-    {
-      description:
-        "Introducing the Teslo Chill Collection. The Women's Chill Half Zip Cropped Hoodie has a premium, soft fleece exterior and cropped silhouette for comfort in everyday lifestyle. The hoodie features an elastic hem that gathers at the waist, subtle thermoplastic polyurethane Teslo logos along the hood and on the sleeve, a double layer single seam hood and a custom ring zipper pull. Made from 60% cotton and 40% recycled polyester.",
-      images: ["1740226-00-A_0_2000.jpg", "1740226-00-A_1.jpg"],
-      inStock: 10,
-      price: 130,
-      sizes: ["XS", "S", "M", "XXL"],
-      slug: "women_chill_half_zip_cropped_hoodie",
-      type: "hoodies",
-      tags: ["hoodie"],
-      title: "Women's Chill Half Zip Cropped Hoodie",
-      gender: "women",
-    },
+
     {
       description:
         "Introducing the Teslo Raven Collection. The Women's Raven Slouchy Crew Sweatshirt has a premium, relaxed silhouette made from a sustainable bamboo cotton blend. The slouchy crew features a subtle thermoplastic polyurethane Teslo wordmark on the left sleeve and a french terry interior for a cozy look and feel in every season. Pair it with your Raven Joggers or favorite on the go fit. Made from 70% bamboo and 30% cotton.",
@@ -644,19 +656,6 @@ export const initialData: SeedData = {
       type: "shirts",
       tags: ["shirt"],
       title: "Made on Earth by Humans Onesie",
-      gender: "kid",
-    },
-    {
-      description:
-        "The Kids Scribble T Logo Onesie is made from 100% Peruvian cotton and features a Teslo T sketched logo for every little artist to wear.",
-      images: ["8529387-00-A_0_2000.jpg", "8529387-00-A_1.jpg"],
-      inStock: 0,
-      price: 30,
-      sizes: ["XS", "S"],
-      slug: "scribble_t_logo_onesie",
-      type: "shirts",
-      tags: ["shirt"],
-      title: "Scribble T Logo Onesie",
       gender: "kid",
     },
     {
