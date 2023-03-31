d465efd6587e634612c00cafa14378e644f68905
José Lin (Light-it) <jose.lin@lightit.io>
2023-03-31 10:42:11 -0300

    Add REST endpoint to GET all products

---
+++ pages/api/products/index.ts
@@ -0,0 +1,36 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { db } from "@/database";
+import { Product } from "@/models";
+import { IProduct } from "@/interfaces";
+
+type Data = { message: string } | IProduct[];
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  switch (req.method) {
+    case "GET":
+      return getProducts(req, res);
+
+    default:
+      return res.status(400).json({
+        message: "Bad request",
+      });
+  }
+
+  res.status(200).json({ message: "Example" });
+}
+
+const getProducts = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  await db.connect();
+  const products = await Product.find()
+    .select("title images price inStock slug -_id")
+    .lean()
+    .exec();
+
+  await db.disconnect();
+
+  return res.status(200).json(products);
+};
