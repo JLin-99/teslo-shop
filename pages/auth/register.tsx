392295e7602f000c5319329bd8fa4ab94bf14a5d
José Lin (Light-it) <jose.lin@lightit.io>
2023-04-05 13:26:01 -0300

    Add form validations to RegisterPage

--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -1,50 +1,140 @@
+import { useState } from "react";
+
 import NextLink from "next/link";
 
+import {
+  Box,
+  Button,
+  Chip,
+  Grid,
+  Link,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { ErrorOutline } from "@mui/icons-material";
+import { useForm } from "react-hook-form";
+
 import { AuthLayout } from "@/components/layouts";
-import { Box, Button, Grid, Link, TextField, Typography } from "@mui/material";
+import { validation } from "@/utils";
+import { tesloAPI } from "@/api";
+
+type FormData = {
+  name: string;
+  email: string;
+  password: string;
+};
 
 const RegisterPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+  const [showError, setShowError] = useState(false);
+
+  const onRegisterForm = async (formData: FormData) => {
+    setShowError(false);
+    try {
+      const { data } = await tesloAPI.post("/user/register", formData);
+      const { token, user } = data;
+      console.log({ token, user });
+    } catch (error) {
+      console.log("Invalid credentials");
+      setShowError(true);
+      setTimeout(() => {
+        setShowError(false);
+      }, 3000);
+    }
+  };
+
   return (
     <AuthLayout title={"Register"}>
-      <Box sx={{ width: 350, padding: "10px 20px" }}>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <Typography variant="h1" component="h1">
-              Create account
-            </Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <TextField label="Name" variant="filled" fullWidth />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField label="Email" variant="filled" fullWidth />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              label="Password"
-              type="password"
-              variant="filled"
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <Button
-              color="secondary"
-              className="circular-btn"
-              size="large"
-              fullWidth
-            >
-              Log In
-            </Button>
-          </Grid>
+      <form onSubmit={handleSubmit(onRegisterForm)} noValidate>
+        <Box sx={{ width: 350, padding: "10px 20px" }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Typography variant="h1" component="h1">
+                Create account
+              </Typography>
+              <Chip
+                label="Email already in use"
+                color="error"
+                icon={<ErrorOutline />}
+                className="fadeIn"
+                sx={{ display: showError ? "flex" : "none" }}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label="name"
+                variant="filled"
+                fullWidth
+                {...register("name", {
+                  required: "This field is required",
+                  minLength: {
+                    value: 2,
+                    message: "Must be at least 2 characters",
+                  },
+                  maxLength: {
+                    value: 50,
+                    message: "Must be less than 50 characters",
+                  },
+                })}
+                error={!!errors.name}
+                helperText={errors.name?.message}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                type="email"
+                label="Email"
+                variant="filled"
+                fullWidth
+                {...register("email", {
+                  required: "This field is required",
+                  validate: validation.isEmail,
+                })}
+                error={!!errors.email}
+                helperText={errors.email?.message}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label="Password"
+                type="password"
+                variant="filled"
+                fullWidth
+                {...register("password", {
+                  required: "This field is required",
+                  minLength: {
+                    value: 6,
+                    message: "Must be at least 6 characters",
+                  },
+                })}
+                error={!!errors.password}
+                helperText={errors.password?.message}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                type="submit"
+                color="secondary"
+                className="circular-btn"
+                size="large"
+                fullWidth
+              >
+                Register
+              </Button>
+            </Grid>
 
-          <Grid item xs={12} display="flex" justifyContent="flex-end">
-            <NextLink href="/auth/login" passHref legacyBehavior>
-              <Link underline="always">Already have an account?</Link>
-            </NextLink>
+            <Grid item xs={12} display="flex" justifyContent="flex-end">
+              <NextLink href="/auth/login" passHref legacyBehavior>
+                <Link underline="always">Already have an account?</Link>
+              </NextLink>
+            </Grid>
           </Grid>
-        </Grid>
-      </Box>
+        </Box>
+      </form>
     </AuthLayout>
   );
 };
