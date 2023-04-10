31acd975d2a1b7c9adb4c4b275c08b260b7bd242
José Lin (Light-it) <jose.lin@lightit.io>
2023-04-10 18:40:11 -0300

    Add NextAuth custom login

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,15 +1,36 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import Credentials from "next-auth/providers/credentials";
 
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
+    Credentials({
+      name: "Custom Login",
+      credentials: {
+        emails: {
+          label: "Email",
+          type: "email",
+          placeholder: "email@example.com",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Password",
+        },
+      },
+      async authorize(credentials) {
+        return null;
+      },
+    }),
+
     GithubProvider({
       clientId: process.env.GITHUB_ID || "",
       clientSecret: process.env.GITHUB_SECRET || "",
     }),
-    // ...add more providers here
   ],
+
+  callbacks: {},
 };
 
 export default NextAuth(authOptions);
