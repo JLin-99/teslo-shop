29f74bf51d014f315e26e1241ef1b15f5aa573ff
José Lin (Light-it) <jose.lin@lightit.io>
2023-04-11 13:23:04 -0300

    Update middleware with NextAuth

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,31 @@
 import { NextRequest, NextResponse } from "next/server";
-import { jwtVerify } from "jose";
+import { getToken } from "next-auth/jwt";
 
 export async function middleware(req: NextRequest) {
-  if (req.nextUrl.pathname.startsWith("/checkout/")) {
-    const token = req.cookies.get("token")?.value || "";
+  // if (req.nextUrl.pathname.startsWith("/checkout/")) {
+  //   const token = req.cookies.get("token")?.value || "";
 
-    try {
-      await jwtVerify(
-        token,
-        new TextEncoder().encode(process.env.JWT_SECRET_SEED)
-      );
-      return NextResponse.next();
-    } catch (error) {
-      return NextResponse.redirect(
-        new URL(`/auth/login?p=${req.nextUrl.pathname}`, req.url)
-      );
-    }
+  //   try {
+  //     await jwtVerify(
+  //       token,
+  //       new TextEncoder().encode(process.env.JWT_SECRET_SEED)
+  //     );
+  //     return NextResponse.next();
+  //   } catch (error) {
+  //     return NextResponse.redirect(
+  //       new URL(`/auth/login?p=${req.nextUrl.pathname}`, req.url)
+  //     );
+  //   }
+  // }
+
+  const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+  if (!session) {
+    const requestedPage = req.nextUrl.pathname;
+    const url = req.nextUrl.clone();
+    url.pathname = `/auth/login`;
+    url.search = `p=${requestedPage}`;
+
+    return NextResponse.redirect(url);
   }
 
   return NextResponse.next();
