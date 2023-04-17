5d2249f524924cb6ba7462fc4a38fc2396de0af4
José Lin (Light-it) <jose.lin@lightit.io>
2023-04-17 16:03:01 -0300

    Verify paid order

--- pages/api/orders/pay.ts
+++ pages/api/orders/pay.ts
@@ -2,6 +2,10 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 import axios from "axios";
 
+import { db } from "@/database";
+import { IPaypal } from "@/interfaces";
+import { Order } from "@/models";
+
 type Data = {
   message: string;
 };
@@ -60,5 +64,42 @@ const payOrder = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
     return res.status(400).json({ message: "Could not get bearer token" });
   }
 
-  return res.status(200).json({ message: paypalBearerToken });
+  const { transactionId = "", orderId = "" } = req.body;
+
+  const { data } = await axios.get<IPaypal.PaypalOrderStatusResponse>(
+    `${process.env.PAYPAL_ORDERS_URL}/${transactionId}`,
+    {
+      headers: {
+        Authorization: `Bearer ${paypalBearerToken}`,
+      },
+    }
+  );
+
+  if (data.status !== "COMPLETED") {
+    return res.status(401).json({ message: "Order not recognized" });
+  }
+
+  await db.connect();
+  const dbOrder = await Order.findById(orderId);
+
+  if (!dbOrder) {
+    await db.disconnect();
+    return res
+      .status(400)
+      .json({ message: "Order does not exist in the database" });
+  }
+
+  if (dbOrder.total !== Number(data.purchase_units[0].amount.value)) {
+    await db.disconnect();
+    return res
+      .status(400)
+      .json({ message: "PayPal amount and database amount are not the same" });
+  }
+
+  dbOrder.transactionId = transactionId;
+  dbOrder.isPaid = true;
+  await dbOrder.save();
+  await db.disconnect();
+
+  return res.status(200).json({ message: "Paid order" });
 };
